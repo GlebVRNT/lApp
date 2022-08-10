51ec2a23330bee94ce424184a886ed80b1018643
Aleksandr Štšepelin <as@jp.ee>
2022-08-10 23:25:56 +0200

    fixed: models

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -4,18 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Banner extends Model
 {
-    protected $fillable = [
-        'name',
-        'user_id',
-        'target_url',
-        'img_url',
-        'cpm',
-        'views_limit'
-    ];
-
     use HasFactory;
-    
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array<string>|bool
+     */
+    protected $guarded = [];
+
+    /**
+     * Get the user for the banner.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
